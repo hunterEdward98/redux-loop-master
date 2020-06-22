64adaec675eaf135eb271f870d740dc1f89821e5
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-22 09:37:38 -0500

    comments

--- server/routes/feedbackRouter.js
+++ server/routes/feedbackRouter.js
@@ -7,7 +7,7 @@ router.post('/', (req, res) => {
     const val2 = req.body.understanding;
     const val3 = req.body.support;
     const val4 = req.body.comments;
-    //server sending 500. no time to fix
+    //server sending 500. no time to fix OOF.
     let queryText = 'INSERT INTO feedback (feeling, understanding, support, comments) VALUES($1,$2,$3,$4)';
     pool.query(queryText, [val1, val2, val3, val4]).then((response) => {
         res.sendStatus(201);

abb62a61cd1e9d7ee187eeee6f09fb3cf21aa33e
Hunter Edward <Hunter.scheel@outlook.com>
2020-06-22 09:50:56 -0500

    got server working

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -7,12 +7,15 @@ router.post('/', (req, res) => {
     const val2 = req.body.understanding;
     const val3 = req.body.support;
     const val4 = req.body.comments;
+    console.log(val1, val2, val3, val4)
     //server sending 500. no time to fix OOF.
     let queryText = 'INSERT INTO feedback (feeling, understanding, support, comments) VALUES($1,$2,$3,$4)';
     pool.query(queryText, [val1, val2, val3, val4]).then((response) => {
-        res.sendStatus(201);
+        console.log('success')
+        res.sendStatus(response);
     }).catch((error) => {
-        res.sendStatus('ERROR', 500);
+
+        res.sendStatus(500);
     });
 }); // END PUT Route
 module.exports = router;
